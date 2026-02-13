a8ebc708d0e8b26e179f33c16c1de2263ee1a939
gadieen <gadieenkhemkaar999@gmail.com>
2026-02-13 08:04:33 +0530

    did basics of tailwind

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div class="flex flex-col items-center p-7 rounded-2xl">
+        <div>
+          <img
+            class="size-48 shadow-xl rounded-md"
+            alt=""
+            src="/img/cover.png"
+          />
+        </div>
+        <div class="flex items-center">
+          <span class="text-2xl font-medium">Class Warfare</span>
+          <span class="font-medium text-sky-500">The Anti-Patterns</span>
+          <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+            <span>No. 4</span>
+            <span>·</span>
+            <span>2025</span>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
